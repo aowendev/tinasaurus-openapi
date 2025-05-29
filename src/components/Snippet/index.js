5881a536b3560aa0564da85a875c5cbb770fe7f9
aowendev <aowen@translationcommons.org>
2025-05-29 18:08:53 +0100

    revert to last working build

--- src/components/Snippet/index.js
+++ src/components/Snippet/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 
 const Snippet = ({ filepath }) => {
-  let lang = document.documentElement.lang;
   let SnippetMDX;
   try {
-    if (lang !== "en") {
-      SnippetMDX = require(`@site/reuse/snippets/i18n/${lang}/${filepath}`).default;
-    } else {
-      SnippetMDX = require(`@site/reuse/snippets/${filepath}`).default;
-    }
+    SnippetMDX = require(`@site/reuse/snippets/${filepath}`).default;
   } catch (e) {
-      return <div>Error: Snippet not found.</div>;
+    return <div>Error: Snippet not found.</div>;
   }
 
-  return <SnippetMDX />;
+  // Render the imported MDX component directly
+  return ( <SnippetMDX /> );
 };
 
 export default Snippet;
