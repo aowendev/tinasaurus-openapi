3ea2cdf7c2dea634065e234c2d8b7f289185efdc
aowendev <aowen@translationcommons.org>
2025-05-29 20:12:40 +0100

    rework snippets

--- src/components/Snippet/index.js
+++ src/components/Snippet/index.js
@@ -3,7 +3,11 @@ import React from "react";
 const Snippet = ({ filepath }) => {
   let SnippetMDX;
   try {
-    SnippetMDX = require(`@site/reuse/snippets/${filepath}`).default;
+    if (lang !== "en") {
+      SnippetMDX = require(`@site/i18n/${lang}/snippets/${filepath}`).default;
+    } else {
+      SnippetMDX = require(`@site/reuse/snippets/${filepath}`).default;
+}
   } catch (e) {
     return <div>Error: Snippet not found.</div>;
   }
